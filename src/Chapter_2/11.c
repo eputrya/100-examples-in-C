652a1fedc4723d45d3efe4a708ab7a3a2316dc41
Evgeniy <garden8b103@gmail.com>
2023-11-07 22:21:28 +0300

    Листинг 11. Символ гласный или согласный

---
+++ src/Chapter_2/11.c
@@ -0,0 +1,28 @@
+/* Пример 11. Проверяем является ли символ гласным или согласныи */
+#include <stdio.h>
+
+int main()
+{
+	char c;
+	int isLowercaseVowel, isUppercaseVowel;
+
+	printf("Введите символ: ");
+	scanf("%c", &c);
+
+	/* возвращает 1 (true), если с - гласный (нижний регистр) */
+	  isLowercaseVowel = (c == 'а' || c == 'е' || c == 'и' || c == 'о'
+	  || c == 'у' || c == 'э' || c == 'ю' || c == 'я' || c == 'ы'); 
+
+	/* возвращает 1 (true), если с - гласный (верхний регистр) */
+	  isUppercaseVowel = (c == 'А' || c == 'Е' || c == 'И' || c == 'О'
+	  || c == 'У' || c == 'Э' || c == 'Ю' || c == 'Я' || c == 'Ы'); 
+
+	/* возвращает 1 (true), если true одна из переменных - isLowercaseVowel
+	 * или isUppercaseVowel */
+
+	if (isLowercaseVowel || isUppercaseVowel)
+			printf("Введенный сивол - гласный\n");
+	else
+			printf("Введенный символ - согласный\n");
+	return 0;
+}
