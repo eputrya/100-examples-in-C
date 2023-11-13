952ba440a0aa5319203edfb38da62356d636b242
Evgeniy <garden8b103@gmail.com>
2023-11-13 21:10:40 +0300

    Демонстрация цикла do..while

---
+++ src/Chapter_2/16-3.c
@@ -0,0 +1,22 @@
+/* Демонстрация циклв do..while */
+#include <stdio.h>
+int main()
+{
+    int n, i, sum = 0;
+
+    do {
+        printf("Введите целое положительное число > 0: ");
+        scanf("%d", &n);
+    }
+
+    while(n <= 0);
+
+    for(i=1; i <= n; ++i)
+    {
+        sum += i; // sum = sum+i
+    }
+
+    printf("Сумма = %d", sum);
+
+    return 0;
+}
