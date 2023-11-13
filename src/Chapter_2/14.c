54324f71dd72a18953958096b3deef1261160a19
Evgeniy <garden8b103@gmail.com>
2023-11-13 20:53:00 +0300

    Високосный год?

---
+++ src/Chapter_2/14.c
@@ -0,0 +1,28 @@
+/* Високосный год? */
+#include <stdio.h>
+
+int main()
+{
+    int year;
+
+    printf("Введите год: ");
+    scanf("%d", &year);
+
+    if(year%4 == 0)
+    {
+        if(year%100 == 0)
+        {
+            // year делится на 400, поэтому високосный
+            if(year%400 == 0)
+                printf("%d - високосный\n", year);
+            else
+                printf("%d - невисокосный\n", year);
+        }
+        else
+            printf("%d - високосный\n", year);
+    }
+    else
+        printf("%d - невисокосный\n",  year);
+
+    return 0;
+}
