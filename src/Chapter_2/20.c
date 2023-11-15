d0731b2a9c93d97cac86c557f1d80662eb09467e
Evgeniy <garden8b103@gmail.com>
2023-11-15 20:56:56 +0300

    Програма, генерирующая полноценную таблицу умножения

---
+++ src/Chapter_2/20.c
@@ -0,0 +1,16 @@
+/* Программа, генерирующая полноценную таблицу умножения */
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for(i = 1; i <= 9; i++)
+    {
+        for(j = 1; j <= 9; j++)
+            printf("%2d  ", i*j);
+        printf("\n");
+    }
+
+    return 0;
+}
