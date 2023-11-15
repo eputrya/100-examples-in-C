61d6f11e6c77b4f33dc83262f1907f57bf9c404c
Evgeniy <garden8b103@gmail.com>
2023-11-15 20:53:25 +0300

    Выводим таблицу умножения

---
+++ src/Chapter_2/19.c
@@ -0,0 +1,17 @@
+/* Выводим таблицу умножения */
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+
+    printf("Введите число: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= 10; ++i)
+    {
+        printf("%d * %d = %d \n", n, i, n * i);    
+    }
+
+    return 0;
+}
