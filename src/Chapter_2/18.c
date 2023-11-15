ee742a2a91b5b720e8d61b72dad3ffcaae1eba8d
Evgeniy <garden8b103@gmail.com>
2023-11-15 20:47:14 +0300

    Вычисляем факториал

---
+++ src/Chapter_2/18.c
@@ -0,0 +1,25 @@
+/* Вычисляем факториал */
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    unsigned long long factorial = 1;
+
+    printf("Введите n: ");
+    scanf("%d", &n);
+
+    // проверяем, чтобы введеное число было положительным
+    if (n < 0)
+        printf("Факториал отрицательного числа не существует.\n");
+    else
+    {
+        for(i = 1; i <= n; ++i)
+            {
+                factorial *= i;
+            }
+        printf("Факториал %d = %llu\n", n, factorial);
+    }
+
+    return 0;
+}
