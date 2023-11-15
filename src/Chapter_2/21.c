6daaa3268b86648fc83ab6b4f7887d7c4156b7f0
Evgeniy <garden8b103@gmail.com>
2023-11-15 21:12:40 +0300

    Вычисление последовательности Фибоначчи

---
+++ src/Chapter_2/21.c
@@ -0,0 +1,31 @@
+/* Вычисление последовательности Фибоначчи */
+#include <stdio.h>
+int main()
+{
+    int i, n, t1 = 0, t2 = 1, nextTerm = 0;
+    printf("Введите количество элементов последовательности: ");
+    scanf("%d", &n);
+
+    printf("Последовательность Фибоначчи: ");
+
+    for(i = 1; i <= n; ++i)
+    {
+        if(i == 1)
+        {
+            printf("%d ", t1);
+            continue;
+        }
+        if(i == 2)
+        {
+            printf("%d ", t2);
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        printf("%d ", nextTerm);
+    }
+
+    printf("\n");
+    return 0;
+}
