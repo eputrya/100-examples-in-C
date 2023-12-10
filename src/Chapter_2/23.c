77a7ce1ed4422ce313e13fdd8f41af10a404d7fb
Evgeniy <garden8b103@gmail.com>
2023-12-10 14:39:39 +0300

    Определяем количество цифр в целом числе

---
+++ src/Chapter_2/23.c
@@ -0,0 +1,19 @@
+/* Определяем количество цифр в целом числе */
+#include <stdio.h>
+int main()
+{
+    long long n;
+    int count = 0;
+
+    printf("Введите целое число: ");
+    scanf("%lld", &n);
+
+    while (n != 0)
+    {
+        // n = n / 10
+        n /= 10;
+        ++count;
+    }
+
+    printf("Количество цифр: %d\n", count);
+}
