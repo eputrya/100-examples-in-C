19b4cdd92690e2c504aedd565b994977194ce415
Evgeniy <garden8b103@gmail.com>
2023-12-16 18:19:31 +0300

    Выводим простые числа

---
+++ src/Chapter_2/28-b.c
@@ -0,0 +1,38 @@
+/* Выводим простые числа */
+#include <stdio.h>
+
+int main()
+{
+    int low, high, i, flag;
+    printf("Выводим простые числа между a и b, введите a и b: ");
+    scanf("%d %d", &low, &high);
+
+    if (low > high)
+    {
+        int temp = low;
+        low = high;
+        high = temp;
+    }
+
+    while (low < high)
+    {
+        flag = 0;
+
+        for (i = 2; i < low / 2; ++i)
+        {
+            if (low % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+            printf("%d ", low);
+
+        ++low;
+    }
+
+    printf("\n");
+    return 0;
+}
