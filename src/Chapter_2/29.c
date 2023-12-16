4645f4d1ed6a374702bc6369adc35e26bb974ff1
Evgeniy <garden8b103@gmail.com>
2023-12-16 18:27:19 +0300

    Является ли число числом Армстронга

---
+++ src/Chapter_2/29.c
@@ -0,0 +1,35 @@
+/* Является ли число числом Армстронга */
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int number, originalNumber, remainder, result = 0, n = 0;
+
+    printf("Введите число: ");
+    scanf("%d", &number);
+
+    originalNumber = number;
+
+    while (originalNumber != 0)
+    {
+        originalNumber /= 10;
+        ++n;
+    }
+
+    originalNumber = number;
+
+    while (originalNumber != 0)
+    {
+        remainder = originalNumber % 10;
+        result += pow(remainder, n);
+        originalNumber /= 10;
+    }
+
+    if (result == number)
+        printf("%d - число Армстронга\n", number);
+    else
+        printf("%d - не является числом Армстронга\n", number);
+
+    return 0;
+}
