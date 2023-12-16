dc6c0498e69fe5f8fdad6c2fc1110147ed491ac7
Evgeniy <garden8b103@gmail.com>
2023-12-16 18:13:06 +0300

    Выводим простые числа между a и b (между low и high)

---
+++ src/Chapter_2/28.c
@@ -0,0 +1,30 @@
+/* Выводим все простые числа между a и b (между low и high) */
+#include <stdio.h>
+int main()
+{
+    int low, high, i, flag;
+    printf("Выводим простые числа между a и b, введите a и b: ");
+    scanf("%d %d", &low, &high);
+
+    while (low < high)
+    {
+        flag = 0;
+
+        for (i = 2; i <= low / 2; ++i)
+        {
+            if (low % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+            printf("%d ", low);
+
+        ++low;
+    }
+
+    printf("\n");
+    return 0;
+}
