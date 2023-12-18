a09ff4b9707bc1d9831b943a758fb309f944e80e
Evgeniy <garden8b103@gmail.com>
2023-12-18 21:33:34 +0300

    Может ли число быть выраженным в виде суммы простых чисел

---
+++ src/Chapter_3/35.c
@@ -0,0 +1,50 @@
+/* Может ли число быть выраженным в виде суммы простых чисел */
+#include <stdio.h>
+
+int checkPrime(int n);
+
+int main()
+{
+    int n, i, flag = 0;
+
+    printf("Введите целое положительное число: ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n / 2; ++i)
+    {
+        // проверяем, является ли i простым
+        if (checkPrime(i) == 1)
+        {
+            // является  ли n-i простым
+            if (checkPrime(n - i) == 1)
+            {
+                // n = primeNumber1 + primeNumber2
+                printf("%d = %d + %d\n", n, i, n - i);
+                flag = 1;
+            }
+        }
+    }
+
+    if (flag == 0)
+        printf("%d не может быть выражено как сумма простых чисел.", n);
+    printf("\n");
+
+    return 0;
+}
+
+// Function to check prime number
+int checkPrime(int n)
+{
+    int i, isPrime = 1;
+
+    for (i = 2; i <= n / 2; ++i)
+    {
+        if (n % i == 0)
+        {
+            isPrime = 0;
+            break;
+        }
+    }
+
+    return isPrime;
+}
