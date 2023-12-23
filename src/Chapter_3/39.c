8fcb81332806dc862c81d67e4ac0db74f1ba9e43
Evgeniy <garden8b103@gmail.com>
2023-12-23 22:03:41 +0300

    Преобразование между двоичной и десятичной системами счисления

---
+++ src/Chapter_3/39.c
@@ -0,0 +1,50 @@
+/* Преобразование между двоичной и десятичной системами счисления */
+#include <stdio.h>
+#include <math.h>
+
+int convertBinaryToDecimal(long long n);
+long long convertDecimalToBinary(int n);
+
+int main()
+{
+    long long n;
+    printf("Введите двоичное число: ");
+    scanf("%lld", &n);
+    printf("%lld (двоичное) = %d (десятичное) \n", n, convertBinaryToDecimal(n));
+
+    printf("Введите десятичное число: ");
+    scanf("%lld", &n);
+
+    printf("%lld (десятичное) = %lld (двоичное) \n", n, convertDecimalToBinary(n));
+    return 0;
+}
+
+int convertBinaryToDecimal(long long n)
+{
+    int decimalNumber = 0, i = 0, remainder;
+    while (n != 0)
+    {
+        remainder = n % 10;
+        n /= 10;
+        decimalNumber += remainder * pow(2, i);
+        ++i;
+    }
+    return decimalNumber;
+}
+
+long long convertDecimalToBinary(int n)
+{
+    long long binaryNumber = 0;
+    int remainder, i = 1, step = 1;
+
+    while (n != 0)
+    {
+        remainder = n % 2;
+        printf("Шаг %d: %d/2 Остаток = %d, Частное = %d\n", step++, n, remainder, n / 2);
+        n /= 2;
+        binaryNumber += remainder * i;
+        i *= 10;
+    }
+
+    return binaryNumber;
+}
