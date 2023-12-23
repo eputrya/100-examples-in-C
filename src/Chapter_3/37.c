c2a8a98cae6b6e68757c2e9440f8dcfc7adbd9b4
Evgeniy <garden8b103@gmail.com>
2023-12-23 21:39:29 +0300

    Вычисление факторала с использованием рекурсии

---
+++ src/Chapter_3/37.c
@@ -0,0 +1,20 @@
+/* Вычисление факториала с использованием рекурсии */
+#include <stdio.h>
+long int fact(int n);
+
+int main()
+{
+    int n;
+    printf("Введите положительное число: ");
+    scanf("%d", &n);
+    printf("Факториал %d = %ld\n", n, fact(n));
+    return 0;
+}
+
+long int fact(int n)
+{
+    if (n >= 1)
+        return n * fact(n - 1);
+    else
+        return 1;
+}
