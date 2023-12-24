cfd4b7195240e1e3505558e0a4633b8d39fefb98
Evgeniy <garden8b103@gmail.com>
2023-12-24 20:10:18 +0300

    Конвертируем восьмеричные числа в десятичные и наоборот

---
+++ src/Chapter_3/40.c
@@ -0,0 +1,54 @@
+/* Конвертируем восьмеричные числа в десятичные и наоборот */
+#include <stdio.h>
+#include <math.h>
+
+int convertDecimalToOctal(int decimalNumber);
+long long convertOctalToDecimal(int octalNumber);
+
+int main()
+{
+    int decimalNumber;
+    int octalNumber;
+
+    printf("Введите десятичное число: ");
+    scanf("%d", &decimalNumber);
+
+    printf("%d (десятичное) = %d (восьмеричное)\n", decimalNumber, convertDecimalToOctal(decimalNumber));
+
+    printf("Введите восьмиричное число: ");
+    scanf("%d", &octalNumber);
+
+    printf("%d (восьмиричное) = %lld (десятичное) \n", octalNumber, convertOctalToDecimal(octalNumber));
+
+    return 0;
+}
+
+int convertDecimalToOctal(int decimalNumber)
+{
+    int octalNumber = 0, i = 1;
+
+    while (decimalNumber != 0)
+    {
+        octalNumber += (decimalNumber % 8) * i;
+        decimalNumber /= 8;
+        i *= 10;
+    }
+
+    return octalNumber;
+}
+
+long long convertOctalToDecimal(int octalNumber)
+{
+    int decimalNumber = 0, i = 0;
+
+    while (octalNumber != 0)
+    {
+        decimalNumber += (octalNumber % 10) * pow(8, i);
+        ++i;
+        octalNumber /= 10;
+    }
+
+    i = 1;
+
+    return decimalNumber;
+}
