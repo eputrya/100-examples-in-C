e57d2c9c4278009907079e61317b67646bd22945
Evgeniy <garden8b103@gmail.com>
2023-12-24 22:42:12 +0300

    Выводим предложение в обратном порядке

---
+++ src/Chapter_3/42.c
@@ -0,0 +1,24 @@
+/* Выводим предложение в обратном порядке */
+#include <stdio.h>
+void reverseSentence();
+
+int main()
+{
+    printf("Введите предложение: ");
+    reverseSentence();
+    printf("\n");
+
+    return 0;
+}
+
+void reverseSentence()
+{
+    char c;
+    scanf("%c", &c);
+
+    if (c != '\n')
+    {
+        reverseSentence();
+        printf("%c", c);
+    }
+}
