a14bb3f5009fa9caf1d93dd5281d64f2fc48b067
Evgeniy <garden8b103@gmail.com>
2024-01-11 20:54:45 +0300

    Поиск частоты знаков в строке

---
+++ src/Chapter_5/53.c
@@ -0,0 +1,24 @@
+/* Поиск частоты знаков в строке */
+#include <stdio.h>
+
+int main()
+{
+    char str[1000], ch;
+    int i, frequency = 0;
+
+    printf("Введите строку: ");
+    getc(str);
+
+    printf("Введите символ: ");
+    scanf("%c", &ch);
+
+    for (i = 0; str[i] != '\0'; ++i)
+    {
+        if (ch == str[i])
+            ++frequency;
+    }
+
+    printf("Частота = %d\n", frequency);
+
+    return 0;
+}
