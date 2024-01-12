a6970d843af7e343c0131dc161b419e954831ffd
Evgeniy <garden8b103@gmail.com>
2024-01-12 20:51:51 +0300

    Статистика о строке

---
+++ src/Chapter_5/54.c
@@ -0,0 +1,32 @@
+/* Статистика о строке */
+#include <stdio.h>
+
+int main()
+{
+    char line[150];
+    int i, total, digits, spaces;
+
+    total = digits = spaces = 0;
+
+    printf("Введите строку ");
+    scanf("%[^\n]", line);
+
+    for (i = 0; i < line[i] != '\0'; ++i)
+    {
+        if (line[i] >= '0' && line[i] <= '9')
+        {
+            digits++;
+        }
+        else if (line[i] == ' ')
+        {
+            ++spaces;
+        }
+        +total;
+    }
+
+    printf("Всего символов: %d", total);
+    printf("\nЦифр: %d", digits);
+    printf("\nПробелов: %d", spaces);
+
+    return 0;
+}
