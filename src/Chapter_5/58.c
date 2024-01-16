831ade70517733b2059069e032aecd3c37ea5582
Evgeniy <garden8b103@gmail.com>
2024-01-16 20:51:17 +0300

    Копирование строки без strcpy()

---
+++ src/Chapter_5/58.c
@@ -0,0 +1,19 @@
+/* Копирование строки без strcpy() */
+#include <stdio.h>
+int main()
+{
+    char s1[100], s2[100], i;
+
+    printf("Введите строку s1: ");
+    getc(s1);
+
+    for (i = 0; s[i] != '\0'; ++i)
+    {
+        s2[i] = s1[i];
+    }
+
+    s2[i] = '\0';
+    printf("Строка s2: %s\n", s2);
+
+    return 0;
+}
