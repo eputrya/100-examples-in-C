95f92516d8014eda3ef49a69dd55f9cf488a1295
Evgeniy <garden8b103@gmail.com>
2024-01-18 20:12:26 +0300

    Храним информацию о студенте в структуре

---
+++ src/Chapter_6/60.c
@@ -0,0 +1,33 @@
+/* Храним информацию о студенте в структуре */
+#include <stdio.h>
+struct student
+{
+    char name[50];
+    int roll;
+    int group;
+} s;
+
+int main()
+{
+    printf(" Введите информацию:\n");
+
+    printf("Имя: ");
+    scanf("%s", s.name);
+
+    printf("Номер билеты: ");
+    scanf("%s", &s.roll);
+
+    printf("Номер группы: ");
+    scanf("%f", s.group);
+
+    printf("Выводим информацию:\n");
+
+    printf("Имя: ");
+    puts(s.name);
+
+    printf("Номер билета: %d\n", s.roll);
+
+    printf("Номер группы: %.1f\n", s.group);
+
+    return 0;
+}
