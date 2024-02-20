0c7c3b8bb965826355151f7bb603e3f92a3a6cf5
admin <admin@localhost.localdomain>
2024-02-20 12:49:48 +0300

    Динамическое выделение памяти

---
+++ src/Chapter_6/64.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct course {
+  int marks;
+  char subject[30];
+};
+
+int main() {
+  struct course *ptr;
+  int i, noOfRecords;
+  printf("Количество записей: ");
+  scanf("%d", &noOfRecords);
+
+  // Выделяем память, ptr будет указывать на базовый адрес
+  ptr = (struct course *)malloc(noOfRecords * sizeof(struct course));
+
+  for (i = 0; i < noOfRecords; ++i) {
+    printf("Введите название предмета и оценку\n");
+    scanf("%s %d", &(ptr + i)->subject, &(ptr + i)->marks);
+  }
+
+  printf("Результат: \n");
+
+  for (i = 0; i < noOfRecords; ++i)
+    printf("%s\t%d\n", (ptr + i)->subject, (ptr + i)->marks);
+
+  return 0;
+}
