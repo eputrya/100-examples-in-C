f0e3c19c330263bfa75a573683f0d33ac778daea
admin <admin@localhost.localdomain>
2024-02-20 15:26:37 +0300

    Запись предложения в файл

---
+++ src/Chapter_7/65.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h> /* Для функции exit() */
+
+int main() {
+  char sentence[1000];
+  FILE *fptr;
+
+  fptr = fopen("text.txt", "w");
+  if (fptr == NULL) {
+    printf("Ошибка!");
+    exit(1);
+  }
+
+  printf("Введите предложение:\n");
+  gets(sentence);
+
+  fprintf(fptr, "%s", sentence);
+  fclose(fptr);
+
+  return 0;
+}
